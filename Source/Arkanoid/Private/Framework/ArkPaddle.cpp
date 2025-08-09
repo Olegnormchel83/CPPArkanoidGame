8372331dd8932be1c313470d7a716e45d092cfd9
Olegnormchel83 <cranberries2000@gmail.com>
2025-08-09 21:51:15 +0300

    Created paddle

--- Source/Arkanoid/Private/Framework/ArkPaddle.cpp
+++ Source/Arkanoid/Private/Framework/ArkPaddle.cpp
@@ -2,15 +2,58 @@
 
 #include "ArkPaddle.h"
 
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+
 AArkPaddle::AArkPaddle()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollider"));
+	check(BoxCollider);
+
+	SetRootComponent(BoxCollider);
+	BoxCollider->SetBoxExtent(FVector(25.f, 50.f, 25.f));
+	BoxCollider->SetCollisionResponseToAllChannels(ECR_Block);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	check(StaticMesh);
+	StaticMesh->SetupAttachment(GetRootComponent());
+
+	LeftStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftStaticMesh"));
+	check(LeftStaticMesh);
+	LeftStaticMesh->SetupAttachment(StaticMesh);
+	LeftStaticMesh->SetRelativeLocation(FVector(0.f, -50.f, 0.f));
+	LeftStaticMesh->SetAbsolute(false, false, true);
+
+	RightStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightStaticMesh"));
+	check(RightStaticMesh);
+	RightStaticMesh->SetupAttachment(StaticMesh);
+	RightStaticMesh->SetRelativeLocation(FVector(0.f, 50.f, 0.f));
+	RightStaticMesh->SetAbsolute(false, false, true);
+
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	check(Arrow);
+	Arrow->SetupAttachment(StaticMesh);
+	Arrow->SetRelativeLocation(FVector(150.f, 0.f, 0.f));
+	Arrow->SetAbsolute(false, false, true);
 }
 
 void AArkPaddle::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AArkPaddle::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	SetActorScale3D(DefaultScale);
+	BoxCollider->SetBoxExtent(FVector(25.f, 50.f + 20.f / DefaultScale.Y, 25.f));
+	const FVector TempScale = FVector(GetActorScale().X, GetActorScale().X, GetActorScale().Z);
+	LeftStaticMesh->SetWorldScale3D(TempScale);
+	RightStaticMesh->SetWorldScale3D(TempScale);
 }
 
 void AArkPaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- Source/Arkanoid/Public/Framework/ArkPaddle.h
+++ Source/Arkanoid/Public/Framework/ArkPaddle.h
@@ -6,17 +6,62 @@
 
 #include "ArkPaddle.generated.h"
 
+class AArkBall;
+class UArrowComponent;
+class UBoxComponent;
+
 UCLASS()
 class ARKANOID_API AArkPaddle : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components",
+		meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* BoxCollider = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components",
+		meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components",
+		meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* LeftStaticMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components",
+		meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* RightStaticMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components",
+		meta = (AllowPrivateAccess = "true"))
+	UArrowComponent* Arrow = nullptr;
+
+	UPROPERTY()
+	AArkBall* CurrentBall = nullptr;
+	
 public:
 	AArkPaddle();
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
 public:
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game",
+		meta = (ToolTip = "Класс мяча для спауна"))
+	TSubclassOf<AArkBall> BallClass = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game",
+		meta = (ToolTip = "Размер каретки (по умолчанию)"))
+	FVector DefaultScale = FVector(0.4f, 2.4f, 0.5f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game",
+		meta = (ToolTip = "Жизни"))
+	int32 Lives = 3;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Settings | Game",
+		meta = (ToolTip = "Скорость каретки"))
+	float Speed = 2000.f;
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
